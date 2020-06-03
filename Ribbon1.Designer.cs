a21774d83793499f7f42fad4f80e28eb33bc73d8
Liu-Peng@Newton-PC-3 <littleNewton6@outlook.com>
2020-06-04 05:14:17 +0800

    ADD - 第一个能用的版本

---
+++ Ribbon1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Equation_and_Codebox {
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory()) {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.MainTab = this.Factory.CreateRibbonTab();
+            this.EquationGroup = this.Factory.CreateRibbonGroup();
+            this.CodeGroup = this.Factory.CreateRibbonGroup();
+            this.IntroduceGroup = this.Factory.CreateRibbonGroup();
+            this.btn_AboutThisAddIn = this.Factory.CreateRibbonButton();
+            this.btn_InsertEquation = this.Factory.CreateRibbonButton();
+            this.btn_InsertCodeBox = this.Factory.CreateRibbonButton();
+            this.MainTab.SuspendLayout();
+            this.EquationGroup.SuspendLayout();
+            this.CodeGroup.SuspendLayout();
+            this.IntroduceGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainTab
+            // 
+            this.MainTab.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.MainTab.Groups.Add(this.EquationGroup);
+            this.MainTab.Groups.Add(this.CodeGroup);
+            this.MainTab.Groups.Add(this.IntroduceGroup);
+            this.MainTab.Label = "公式与代码";
+            this.MainTab.Name = "MainTab";
+            // 
+            // EquationGroup
+            // 
+            this.EquationGroup.Items.Add(this.btn_InsertEquation);
+            this.EquationGroup.Label = "公式";
+            this.EquationGroup.Name = "EquationGroup";
+            // 
+            // CodeGroup
+            // 
+            this.CodeGroup.Items.Add(this.btn_InsertCodeBox);
+            this.CodeGroup.Label = "代码";
+            this.CodeGroup.Name = "CodeGroup";
+            // 
+            // IntroduceGroup
+            // 
+            this.IntroduceGroup.Items.Add(this.btn_AboutThisAddIn);
+            this.IntroduceGroup.Label = "关于";
+            this.IntroduceGroup.Name = "IntroduceGroup";
+            // 
+            // btn_AboutThisAddIn
+            // 
+            this.btn_AboutThisAddIn.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btn_AboutThisAddIn.Label = "关于插件";
+            this.btn_AboutThisAddIn.Name = "btn_AboutThisAddIn";
+            this.btn_AboutThisAddIn.ShowImage = true;
+            this.btn_AboutThisAddIn.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btn_AboutThisAddIn_Click);
+            // 
+            // btn_InsertEquation
+            // 
+            this.btn_InsertEquation.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btn_InsertEquation.Image = global::Equation_and_Codebox.Properties.Resources.equation_icon;
+            this.btn_InsertEquation.Label = "插入带编号的公式";
+            this.btn_InsertEquation.Name = "btn_InsertEquation";
+            this.btn_InsertEquation.ShowImage = true;
+            this.btn_InsertEquation.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btn_InsertEquation_Click);
+            // 
+            // btn_InsertCodeBox
+            // 
+            this.btn_InsertCodeBox.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btn_InsertCodeBox.Image = global::Equation_and_Codebox.Properties.Resources.CodeTable_icon;
+            this.btn_InsertCodeBox.Label = "从剪贴板插入代码";
+            this.btn_InsertCodeBox.Name = "btn_InsertCodeBox";
+            this.btn_InsertCodeBox.ShowImage = true;
+            this.btn_InsertCodeBox.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btn_InsertCodeBox_Click);
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Word.Document";
+            this.Tabs.Add(this.MainTab);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.MainTab.ResumeLayout(false);
+            this.MainTab.PerformLayout();
+            this.EquationGroup.ResumeLayout(false);
+            this.EquationGroup.PerformLayout();
+            this.CodeGroup.ResumeLayout(false);
+            this.CodeGroup.PerformLayout();
+            this.IntroduceGroup.ResumeLayout(false);
+            this.IntroduceGroup.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab MainTab;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup EquationGroup;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btn_InsertEquation;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup CodeGroup;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btn_InsertCodeBox;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup IntroduceGroup;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btn_AboutThisAddIn;
+    }
+
+    partial class ThisRibbonCollection {
+        internal Ribbon1 Ribbon1 {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
---
+++ Ribbon1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.Office.Tools.Ribbon;
+using Word = Microsoft.Office.Interop.Word;
+using Microsoft.Office.Interop.Word;
+using Microsoft.VisualBasic;
+
+namespace Equation_and_Codebox {
+    public partial class Ribbon1 {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e) {
+
+        }
+
+        /// <summary>
+        /// 插入带有编号的公式
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_InsertEquation_Click(object sender, RibbonControlEventArgs e) {
+            // 创建一个 Word Doc 变量
+            Word.Application app = Globals.ThisAddIn.Application;
+            Word.Document doc = app.ActiveDocument;
+
+            // 在光标处插入一个名为 equationTable 的表格
+            Word.Table equationTable = app.Selection.Tables.Add(app.Selection.Range, 1, 3);
+
+            // 设置 equationTable 每个单元格的具体宽度
+            equationTable.Cell(1, 1).SetWidth(
+                app.CentimetersToPoints(1.75f),
+                Word.WdRulerStyle.wdAdjustNone
+            );
+            equationTable.Cell(1, 2).SetWidth(
+                app.CentimetersToPoints(13.5f),
+                Word.WdRulerStyle.wdAdjustNone
+            );
+            equationTable.Cell(1, 3).SetWidth(
+                app.CentimetersToPoints(1.75f),
+                Word.WdRulerStyle.wdAdjustNone
+            );
+
+            // 设置 equationTable 的整体边界距离为 0
+            equationTable.LeftPadding = 0;
+            equationTable.RightPadding = 0;
+
+            // 居中、居右
+            equationTable.Cell(1, 1).VerticalAlignment = Word.WdCellVerticalAlignment.wdCellAlignVerticalCenter;
+            equationTable.Cell(1, 2).VerticalAlignment = Word.WdCellVerticalAlignment.wdCellAlignVerticalCenter;
+            equationTable.Cell(1, 3).VerticalAlignment = Word.WdCellVerticalAlignment.wdCellAlignVerticalRight;
+
+            // 设置段前、后的距离
+            equationTable.Range.ParagraphFormat.LineUnitBefore = 0;
+            equationTable.Range.ParagraphFormat.LineUnitAfter = 0;
+            equationTable.Range.ParagraphFormat.SpaceBefore = 0;
+            equationTable.Range.ParagraphFormat.SpaceAfter = 0;
+
+        }
+
+
+
+
+
+        /// <summary>
+        /// 显示版权信息
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_AboutThisAddIn_Click(object sender, RibbonControlEventArgs e) {
+            MessageBox.Show("Copyright @刘鹏, 2020,\nEmail: littleNewton6@gmail.com");
+        }
+
+
+
+
+
+        /// <summary>
+        /// 将剪贴板中的内容放入表格，自动生成行号
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_InsertCodeBox_Click(object sender, RibbonControlEventArgs e) {
+            
+            // 创建一个 Word Doc 变量
+            Word.Application app = Globals.ThisAddIn.Application;
+            Word.Document doc = app.ActiveDocument;
+
+            // 在光标处插入一个名为 CodeTable 的表格
+            Word.Table codeTable = app.Selection.Tables.Add(app.Selection.Range, 1, 2);
+
+            // 打开代码单元格的表格边界
+            codeTable.Cell(1, 2).Borders.Enable = 1;
+
+            // 设置单元格宽度
+            codeTable.Cell(1, 1).SetWidth(app.CentimetersToPoints(0.75f), Word.WdRulerStyle.wdAdjustNone);
+            codeTable.Cell(1, 2).SetWidth(app.CentimetersToPoints(16.25f), Word.WdRulerStyle.wdAdjustNone);
+
+            // 整体边界填充
+            codeTable.LeftPadding = app.CentimetersToPoints(0.1f);
+            codeTable.RightPadding = app.CentimetersToPoints(0.1f);
+
+            // 设置段前、后的距离
+            codeTable.Range.ParagraphFormat.LineUnitBefore = 0;
+            codeTable.Range.ParagraphFormat.LineUnitAfter = 0;
+            codeTable.Range.ParagraphFormat.SpaceBefore = 0;
+            codeTable.Range.ParagraphFormat.SpaceAfter = 0;
+
+            // 设置行距
+            codeTable.Range.ParagraphFormat.LineSpacingRule = Word.WdLineSpacing.wdLineSpaceExactly;
+            codeTable.Range.ParagraphFormat.LineSpacing = 12;
+
+            // 设置字体
+            codeTable.Range.Font.NameAscii = "LM Mono 10";
+            codeTable.Range.Font.NameFarEast = "宋体";
+            codeTable.Range.Font.Size = 9;
+
+            // 设置单元格对齐方式
+            codeTable.Cell(1, 1).Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphRight;
+            codeTable.Cell(1, 2).Range.ParagraphFormat.Alignment = Word.WdParagraphAlignment.wdAlignParagraphLeft;
+
+            // 中英文字符间距自动调整：Disable
+            codeTable.Cell(1, 2).Range.ParagraphFormat.AddSpaceBetweenFarEastAndAlpha = 0;
+            codeTable.Cell(1, 2).Range.ParagraphFormat.AddSpaceBetweenFarEastAndDigit = 0;
+
+            // 输入连续的行号
+            string stringNumberOfLines = Microsoft.VisualBasic.Interaction.InputBox("请输入代码终止行数", "输入行数", "50");
+            int NumberOfLines = Int32.Parse(stringNumberOfLines);
+            for (int i = 1; i <= NumberOfLines - 1; i++) {
+                app.Selection.TypeText(i.ToString() + "\n");
+            }
+            app.Selection.TypeText(stringNumberOfLines);
+        }
+    }
+}
