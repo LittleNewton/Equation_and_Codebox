c3ae61f22d1a31fee29e234fa79da92bd3f1bf86
Liu-Peng@Newton-PC-3 <littleNewton6@gmail.com>
2021-12-23 17:44:50 +0800

    New Testing Features : 允许在 .Net 环境里执行 JS 了

--- src/Equation_and_Code/Properties/Resources.Designer.cs
+++ src/Equation_and_Code/Properties/Resources.Designer.cs
@@ -9,6 +9,7 @@
 //------------------------------------------------------------------------------
 
 namespace Equation_and_Code.Properties {
+    using System;
     
     
     /// <summary>
@@ -18,7 +19,7 @@ namespace Equation_and_Code.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -56,6 +57,29 @@ namespace Equation_and_Code.Properties {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to class JSObject {
+        ///    function plus(a, b) {
+        ///        return a + b;
+        ///    }
+        ///}
+        ///.
+        /// </summary>
+        internal static string test_js_plus {
+            get {
+                return ResourceManager.GetString("test_js_plus", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Hello.
+        /// </summary>
+        internal static string test_str {
+            get {
+                return ResourceManager.GetString("test_str", resourceCulture);
             }
         }
     }
